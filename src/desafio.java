b5092f233d45116756552e2d40b1698cb9ee2975
JoaoBJM <joaoboninis5@gmail.com>
2025-03-12 10:30:08 -0300

    Strings iniciais

---
+++ src/desafio.java
@@ -0,0 +1,14 @@
+public class desafio {
+    public static void main(String[] args) {
+        String nome = "joão";
+        String tipoConta = "Corrente";
+                double saldo = 1599.99;
+
+        System.out.println("___________");
+        System.out.println("\nNome do cliente " + nome);
+        System.out.println("Tipo Conta" + tipoConta);
+        System.out.println("Saldo Atual:" + saldo);
+        System.out.println("\n____________");
+
+    }
+}
